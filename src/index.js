b11d336514b14c39a4ec5f950f75718a27598b99
Kujtim Prenku <prenkukujtim.kp@gmail.com>
2019-11-29 20:39:23 +0100

    Setup socket.io server and client side

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io =  socketio(server);
 
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-app.get('/', async (req, res) => {
-    try {
-        res.render('index');
-    } catch (e) {
-        console.log(e);
-    }
+io.on('connection', () => {
+   console.log('New WebSocket connection.')
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log('Server is running on port ' + port);
 });
