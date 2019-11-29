d4a881a17570dbcdc470e9e1409c646d79d49c7a
Kujtim Prenku <prenkukujtim.kp@gmail.com>
2019-11-29 13:53:00 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+const publicDirectoryPath = path.join(__dirname, '../public');
+
+app.use(express.static(publicDirectoryPath));
+
+app.get('/', async (req, res) => {
+    try {
+        res.render('index');
+    } catch (e) {
+        console.log(e);
+    }
+});
+
+app.listen(port, () => {
+    console.log('Server is running on port ' + port);
+});
