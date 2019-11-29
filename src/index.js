d33cf1db3d10660d027db751f7deba9b15026586
Kujtim Prenku <prenkukujtim.kp@gmail.com>
2019-11-29 22:09:16 +0100

    Emit and listen to events

--- src/index.js
+++ src/index.js
@@ -5,15 +5,20 @@ const socketio = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io =  socketio(server);
+const io = socketio(server);
 
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-io.on('connection', () => {
-   console.log('New WebSocket connection.')
+io.on('connection', (socket) => {
+    console.log('New WebSocket connection.');
+    socket.emit('message', 'Welcome');
+
+    socket.on('sendMessage', (message) => {
+        io.emit('message', message);
+    });
 });
 
 server.listen(port, () => {
